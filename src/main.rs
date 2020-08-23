6d4b849b2139d10b811df49202d691bd252a69eb
t-sin <shinichi.tanaka45@gmail.com>
2020-08-23 14:30:44 +0900

    Input can be specified as a file and as stdin

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,11 @@
+extern crate clap;
 extern crate nom;
+
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::{self};
+
+use clap::{App, Arg};
 
 use nom::branch::{alt, permutation};
 use nom::bytes::complete::tag;
@@ -195,7 +202,29 @@ fn parse(s: &str) -> IResult<&str, Token> {
 }
 
 fn main() {
-    let input = "-123.0 + 456.7";
+    let matches = App::new("mmmm")
+        .version("0.1.0")
+        .author("t-sin <shinichi.tanaka45@gmail.com>")
+        .arg(
+            Arg::with_name("filename")
+                .help("Input file name")
+                .required(false),
+        )
+        .get_matches();
+    let mut input = String::new();
+    match matches.value_of("filename") {
+        Some("-") => {
+            let _ = io::stdin().read_to_string(&mut input);
+        }
+        Some(filename) => {
+            let _ = File::open(filename).unwrap().read_to_string(&mut input);
+        }
+        None => {
+            let _ = io::stdin().read_to_string(&mut input);
+        }
+    };
+    let input = &input;
+
     match tokenize(input) {
         Ok((_, tokens)) => println!("parsed: {:?}", tokens),
         err => panic!("parse error: {:?}", err),
