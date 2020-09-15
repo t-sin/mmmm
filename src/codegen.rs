e637afc6fba33d3708d5dac5d499c64a40df8e11
t-sin <shinichi.tanaka45@gmail.com>
2020-09-16 00:53:08 +0900

    Generate code

--- src/codegen.rs
+++ src/codegen.rs
@@ -1,5 +1,103 @@
-use crate::parse::AST;
+use crate::parse::{Declare, Exp, AST};
+use crate::tokenize::{Keyword, Operator, Special};
 
-pub fn generate(_asts: &Vec<AST>) -> String {
-    "".to_string()
+fn generate_exp(exp: &Exp) -> String {
+    match exp {
+        Exp::Float(f) => format!("{}", f),
+        Exp::String(s) => format!("\"{}\"", s),
+        Exp::Special(var) => match **var {
+            Special::Now => format!("now"),
+            Special::SelfVar => format!("self"),
+        },
+        Exp::Variable(name) => format!("{}", name.0),
+        Exp::InvokeFn(name, args) => {
+            let argstr = args
+                .iter()
+                .map(|e| generate_exp(e))
+                .collect::<Vec<_>>()
+                .join(", ");
+            format!("{}({})", name.0, argstr)
+        }
+        Exp::UnaryOp(op, exp) => match **op {
+            Operator::Minus => format!("-{}", generate_exp(exp)),
+            _ => panic!("unknown unary operator {:?}", op),
+        },
+        Exp::BinaryOp(op, exp1, exp2) => {
+            let opstr = op.to_string();
+
+            let exp1str = if let Exp::BinaryOp(_, _, _) = **exp1 {
+                format!("({})", generate_exp(exp1))
+            } else {
+                format!("{}", generate_exp(exp1))
+            };
+            let exp2str = if let Exp::BinaryOp(_, _, _) = **exp2 {
+                format!("({})", generate_exp(exp2))
+            } else {
+                format!("{}", generate_exp(exp2))
+            };
+
+            format!("{} {} {}", exp1str, opstr, exp2str)
+        }
+        Exp::PostOp(op, name, exp) => match **op {
+            Operator::Access => format!("{}[{}]", name.0, generate_exp(exp)),
+            _ => panic!("unknown postfix operator {:?}", op),
+        },
+    }
+}
+
+fn generate_declares(declares: &Vec<Declare>) -> String {
+    let mut decs = Vec::new();
+    for dec in declares.iter() {
+        let s = match dec {
+            Declare::Var(name, Some(kw)) => match kw {
+                Keyword::Float => format!("{}: float", name.0),
+                Keyword::Void => format!("{}: void", name.0),
+                kw => panic!("unknown type name {:?}", kw),
+            },
+            Declare::Var(name, None) => format!("{}", name.0),
+        };
+        decs.push(s);
+    }
+
+    decs.join(", ")
+}
+
+fn generate_1(ast: &AST, nest: u64) -> String {
+    let mut indent = String::new();
+    for _ in 0..nest {
+        indent.push_str("  ");
+    }
+
+    let body = match ast {
+        AST::Exp(exp) => generate_exp(exp),
+        AST::Assign(name, exp) => format!("{} = {}", name.0, generate_exp(exp)),
+        AST::Return(exp) => format!("return {}", generate_exp(exp)),
+        AST::Defun(name, declares, rettype, body) => {
+            let decstr = generate_declares(declares);
+            let retstr = match rettype {
+                Some(Keyword::Float) => format!("-> float"),
+                Some(Keyword::Void) => format!("-> void"),
+                Some(kw) => panic!("unknown type name {:?}", kw),
+                _ => format!(""),
+            };
+            let bodystr = body
+                .iter()
+                .map(|a| generate_1(a, nest + 1))
+                .collect::<Vec<_>>()
+                .join("");
+            format!("fn {}({}) {} {{\n{}}}", name.0, decstr, retstr, bodystr)
+        }
+    };
+
+    format!("{}{}\n", indent, body)
+}
+
+pub fn generate(asts: &Vec<AST>) -> String {
+    let mut code = String::new();
+
+    for ast in asts.iter() {
+        code.push_str(&generate_1(ast, 0));
+    }
+
+    code
 }
