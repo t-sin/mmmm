4567128bba2a3d8bb6a3ebc9477e8b9a8a1c4d41
t-sin <shinichi.tanaka45@gmail.com>
2020-09-21 21:09:27 +0900

    Generate `if` expression codes

--- src/codegen.rs
+++ src/codegen.rs
@@ -5,13 +5,13 @@ fn generate_invoke_fn(invoke: &InvokeFn) -> String {
     let argstr = invoke
         .1
         .iter()
-        .map(|e| generate_exp(e))
+        .map(|e| generate_exp(e, 0))
         .collect::<Vec<_>>()
         .join(", ");
     format!("{}({})", invoke.0.0, argstr)
 }
 
-fn generate_exp(exp: &Exp) -> String {
+fn generate_exp(exp: &Exp, nest: u64) -> String {
     match exp {
         Exp::Float(f) => format!("{}", f),
         Exp::String(s) => format!("\"{}\"", s),
@@ -22,29 +22,52 @@ fn generate_exp(exp: &Exp) -> String {
         Exp::Variable(name) => format!("{}", name.0),
         Exp::InvokeFn(invoke) => generate_invoke_fn(invoke),
         Exp::UnaryOp(op, exp) => match **op {
-            Operator::Minus => format!("-{}", generate_exp(exp)),
+            Operator::Minus => format!("-{}", generate_exp(exp, nest)),
             _ => panic!("unknown unary operator {:?}", op),
         },
         Exp::BinaryOp(op, exp1, exp2) => {
             let opstr = op.to_string();
 
             let exp1str = if let Exp::BinaryOp(_, _, _) = **exp1 {
-                format!("({})", generate_exp(exp1))
+                format!("({})", generate_exp(exp1, nest))
             } else {
-                format!("{}", generate_exp(exp1))
+                format!("{}", generate_exp(exp1, nest))
             };
             let exp2str = if let Exp::BinaryOp(_, _, _) = **exp2 {
-                format!("({})", generate_exp(exp2))
+                format!("({})", generate_exp(exp2, nest))
             } else {
-                format!("{}", generate_exp(exp2))
+                format!("{}", generate_exp(exp2, nest))
             };
 
             format!("{} {} {}", exp1str, opstr, exp2str)
         }
         Exp::PostOp(op, name, exp) => match **op {
-            Operator::Access => format!("{}[{}]", name.0, generate_exp(exp)),
+            Operator::Access => format!("{}[{}]", name.0, generate_exp(exp, nest)),
             _ => panic!("unknown postfix operator {:?}", op),
         },
+        Exp::If(ifexp) => {
+            let true_clause = if ifexp.true_clause.len() > 1 {
+                let mut s = String::new();
+                s.push_str("\n");
+
+                for exp in ifexp.true_clause.iter() {
+                    for _ in 0..nest + 1 {
+                        s.push_str("  ");
+                    }
+
+                    s.push_str(&generate_exp(exp, nest + 1));
+                    s.push_str("\n");
+                }
+                s
+            } else {
+                generate_exp(&ifexp.true_clause[0], nest)
+            };
+            format!(
+                "if ({}) {{{}}}",
+                generate_exp(&ifexp.cond, nest),
+                true_clause
+            )
+        }
     }
 }
 
@@ -72,11 +95,11 @@ fn generate_1(ast: &AST, nest: u64) -> String {
     }
 
     let body = match ast {
-        AST::Exp(exp) => generate_exp(exp),
-        AST::Assign(name, exp) => format!("{} = {}", name.0, generate_exp(exp)),
-        AST::Return(exp) => format!("return {}", generate_exp(exp)),
+        AST::Exp(exp) => generate_exp(exp, nest),
+        AST::Assign(name, exp) => format!("{} = {}", name.0, generate_exp(exp, nest)),
+        AST::Return(exp) => format!("return {}", generate_exp(exp, nest)),
         AST::InvokeAt(invoke, exp) => {
-            format!("{}@{}", generate_invoke_fn(invoke), generate_exp(exp))
+            format!("{}@{}", generate_invoke_fn(invoke), generate_exp(exp, nest))
         }
         AST::Defun(name, declares, rettype, body) => {
             let decstr = generate_declares(declares);
